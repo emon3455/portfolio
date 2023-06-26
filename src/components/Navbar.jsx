8f22e9e21cf10f4a3bd5bbb8e94e007f20bf6843
Emon <emon.mhk69@gmail.com>
2023-06-26 14:43:34 +0600

    downlad resume while clicking button done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,37 +5,24 @@ const Navbar = () => {
         <>
             <div className="navbar bg-base-100">
                 <div className="navbar-start">
-
                     <div className="dropdown">
-
-                        <label className="btn btn-ghost lg:hidden">
+                        <label tabIndex={0} className="btn btn-ghost lg:hidden">
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </label>
-
-                        <ul className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/projects">Projects</Link></li>
                         </ul>
-
                     </div>
-
-                    <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-
-                </div>
-
-                <div className="navbar-center hidden lg:flex">
-
-                    <ul className="menu menu-horizontal px-1">
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/projects">Projects</Link></li>
-                    </ul>
-
+                    <a className="btn btn-ghost normal-case text-xl">Emon</a>
                 </div>
 
                 <div className="navbar-end">
-
-                    <a className="btn">Button</a>
-                    
+                    <ul className="menu menu-horizontal px-1  hidden lg:flex">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/projects">Projects</Link></li>
+                    </ul>
+                    <a href="../../public/_RESUME(Emon)_.pdf" download className="btn btn-sm btn-warning font-bold">Download Resume</a>
                 </div>
             </div>
         </>
